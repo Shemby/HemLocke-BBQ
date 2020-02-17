d9bafd3f08add8746828692f8d61ba29c3d09ae7
Sam <hembysam4@gmail.com>
2020-02-17 13:56:06 -0600

    initial navbar setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from './component/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/Global.scss';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
