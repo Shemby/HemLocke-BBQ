e46a21e4f491dae9b5e442261ef1e9550ddfa346
Sam <hembysam4@gmail.com>
2020-02-23 11:44:34 -0600

    working on contact page layout

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class Contact extends Component {
   render() {
     return (
-      <div className='page'>
+      <div className='page page_contact'>
         <h1 className='heading_main'>Contact Us</h1>
         <div className='card'>
           <h2 className='card_heading'>Make a Reservation</h2>
@@ -28,13 +28,11 @@ export class Contact extends Component {
         </div>
 
         <div className='card_media'>
-          <img src='./images/facebook.svg' alt='' />
-          <img src='./images/instagram.svg' alt='' />
-          <img src='./images/twitter.svg' alt='' />
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Facebook icon</title><path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"/></svg>
         </div>
 
         <form action='submit' className='form_contact'>
-          <input type='text' placeholder='Name' className='form_input-sma' />
+          <input type='text' placeholder='Name' className='form_input-small' />
 
           <input
             type='email'
@@ -53,7 +51,7 @@ export class Contact extends Component {
             placeholder='Message'
             className='form_textarea'
           />
-          <input type='submit' className='form_button-red' value='submit' />
+          <input type='submit' className='form_btn-red' value='submit' />
         </form>
       </div>
     );
